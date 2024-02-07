8c7c866537680c5931627bdc6e362276aa63e2d4
Sergei Semin <povar199513@gmail.com>
2024-02-07 22:21:52 +0300

    дз8

--- hw-js/hv-7.js
+++ hw-js/hv-7.js
@@ -1,4 +1,4 @@
-//задача 1
+// задача 1
 
 // const people = [
 //     { name: 'Глеб', age: 29 },
@@ -14,81 +14,59 @@
 // console.log(people.sort(compareNumbers)) 
  
 
-//задача 2
+// задача 2
 
-// function isPositive(arr) {
-//     return arr.filter(item => item > 0);
-// }
-  
-// function isMale(arr) {
-//     return result = arr.filter(item => item.gender === 'male');
-// }
-  
-// function filter(arr, callback) {
-//     return callback(arr);
-// }
-  
-// const people1 = [
-//      {name: 'Глеб', gender: 'male'},
-//      {name: 'Анна', gender: 'female'},
-//      {name: 'Олег', gender: 'male'},
-//      {name: 'Оксана', gender: 'female'}
-// ];
-  
-// console.log(filter([3, -4, 1, 9], isPositive)); // Должен выводить [3, 1, 9]
-// console.log(filter(people1, isMale)); // Должен выводить [{name: 'Глеб', gender: 'male'},  {name: 'Олег', gender: 'male'}]
-
-//Переделанный код!!! 2 задания
-// function isPositive(arr) {
-//     return arr > 0;
-//   }
-  
-//   function isMale(person) {
-//     return person.gender === 'male';
-//   }
-  
-//   function filter(arr, ruleFunction) {
-//     return arr.map(function(arr ) {
-//       if (ruleFunction(arr )) {
-//         return arr ;
-//       }
-//     }).filter(function(arr ) {
-//       return arr  !== undefined;
-//     });
-//   }
-  
-//   console.log(filter([3, -4, 1, 9], isPositive)); // Выводит: [3, 1, 9]
-  
-//   const people = [
-//      {name: 'Глеб', gender: 'male'},
-//      {name: 'Анна', gender: 'female'},
-//      {name: 'Олег', gender: 'male'},
-//      {name: 'Оксана', gender: 'female'}
-//   ];
-  
-//   console.log(filter(people, isMale)); // Выводит: [{name: 'Глеб', gender: 'male'}, {name: 'Олег', gender: 'male'}]
-
-
-
-
-// //задача 3
-
-// const timer = () => {
-//     const interval = setInterval(() => {
-//         let date = new Date();
-//         console.log(date.toString())
-//     }, 3000)
+function  isPositive(num) {
+    return num > 0;
     
-//     setTimeout(() => {
-//         clearInterval(interval);
-//         console.log('30 секунд прошло')
-//     }, 30000)
+}
+function isMale(person) {
+    return person.gender === 'male';
     
-// }
+}
+function filter(array, ruleFunction) {
+    const result = [];
+    for (let item of array) {
+        if (ruleFunction(item)){
+            result.push(item);
+        }
+        
+        
+    }
+    return result;
+    
+}
+console.log(filter([3, -4, 1, 9], isPositive));
 
-// timer();
 
-// //задача 4
+const people = [
+    {name: 'Глеб', gender: 'male'},
+    {name: 'Анна', gender: 'female'},
+    {name: 'Олег', gender: 'male'},
+    {name: 'Оксана', gender: 'female'}
+ ];
+ 
+ console.log(filter(people, isMale));
+
+
+//задача 3
+
+const timer = () => {
+    const interval = setInterval(() => {
+        let date = new Date();
+        console.log(date.toString())
+    }, 3000)
+    
+    setTimeout(() => {
+        clearInterval(interval);
+        console.log('30 секунд прошло')
+    }, 30000)
+    
+}
+
+timer();
+
+//задача 4
 
 // function delayForSecond(callback) {
 // 	setTimeout(() => {
@@ -110,24 +88,24 @@
 //     console.log('Привет, Глеб!');
 //   });
 
-// //задача 5
+//задача 5
 
-// // Функция delayForSecond через 1 секунду пишет в консоль «Прошла одна секунда», 
-// // а затем вызывает переданный колбэк
-// function delayForSecond(cb) {
-//     setTimeout(() => {
-//         console.log('Прошла одна секунда');
-// 				if(cb) { 	cb(); }
+// Функция delayForSecond через 1 секунду пишет в консоль «Прошла одна секунда», 
+// а затем вызывает переданный колбэк
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+				if(cb) { 	cb(); }
 
-//     }, 1000)
-// }
+    }, 1000)
+}
 
-// // Функция sayHi выводит в консоль приветствие для указанного имени
-// function sayHi (name) {
-//     console.log(`Привет, ${name}!`);
-// }
+// Функция sayHi выводит в консоль приветствие для указанного имени
+function sayHi (name) {
+    console.log(`Привет, ${name}!`);
+}
 
-// // Код выше менять нельзя
+// Код выше менять нельзя
 
-// // Нужно изменить код ниже:
-// delayForSecond(sayHi.bind(null,'Глеб'))
+// Нужно изменить код ниже:
+delayForSecond(sayHi.bind(null,'Глеб'))
