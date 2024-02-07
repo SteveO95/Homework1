328eccd5702c0950847af0cef0164c86bbd00f6f
Sergei Semin <povar199513@gmail.com>
2024-02-07 22:39:48 +0300

    домашнее изменёное задание 8

--- hw-js/hv-7.js
+++ hw-js/hv-7.js
@@ -40,10 +40,12 @@ console.log(filter([3, -4, 1, 9], isPositive));
 
 
 const people = [
+
     {name: 'Глеб', gender: 'male'},
     {name: 'Анна', gender: 'female'},
     {name: 'Олег', gender: 'male'},
     {name: 'Оксана', gender: 'female'}
+    
  ];
  
  console.log(filter(people, isMale));
